30db1e459556ba47e83ff11b748fdcb8dcb00a98
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-09-18 04:31:40 +0530

    upload folder with names

---
+++ website/components/forms/UploadFolder.tsx
@@ -0,0 +1,30 @@
+import { AddCircleSharp } from "@mui/icons-material";
+import { TextField } from "@mui/material";
+import React, { useRef, useState } from "react";
+import ModalContainer from "../common/ModalContainer/ModalContainer";
+
+export default function UploadFolder({
+  open,
+  close,
+  addFolder
+}: any) {
+  const [filename, setFilename] = useState("");
+  const upload = () => {
+    addFolder(filename)
+  }
+  return (
+    <ModalContainer isOpen={open} close={close}>
+      <div className="flex flex-col justify-between h-full w-full align-middle">
+        FileName to be displayed: 
+        <TextField
+          type="string"
+          value={filename}
+          onChange={(e) => setFilename(e.target.value)}
+        />
+        
+        <button onClick={upload}>submit</button>
+        <br />
+      </div>
+    </ModalContainer>
+  );
+}
