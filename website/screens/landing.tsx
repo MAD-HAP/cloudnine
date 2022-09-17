ab24b2ebbcafe1e9322bb6cc32376ccbedab1a29
harsh johar <hpsj3112@gmail.com>
2022-09-17 14:54:06 +0530

    changes

--- website/screens/landing.tsx
+++ website/screens/landing.tsx
@@ -1,11 +1,48 @@
 import { signIn } from "next-auth/react";
+import { useWidth } from "../components/hooks/useWidth";
 
 function Landing() {
-  return (
-    <div>
-        <button onClick={() => signIn()}>Login pls</button>
-    </div>
-  )
+  const width = useWidth();
+    return (
+        <div>
+            <div>
+                {/* TODO */}
+                {/* navbar */}
+                <div className="flex flex-row p-[10px] shadow items-center">
+            {width > 800 ? (
+                <img
+                    src="/cloudnine-big.svg"
+                    className="h-[60px] w-auto rounded-[50%]"
+                    alt="Cloud Nine"
+                />
+            ) : (
+                <img
+                    src="/cloudnine.svg"
+                    className="h-[60px] w-auto rounded-[50%]"
+                    alt="Cloud Nine"
+                />
+            )}
+            {width > 800 && (
+                <div className="text-[20px] ml-[5px] font-bold">Cloud Nine</div>
+            )}
+            
+        </div>
+            </div>
+            <div>
+                {/* TODO */}
+                {/* big splash */}
+                {/* left = text, right = image, left doen = "try Now" */}
+            </div>
+            <div>
+                {/* TODO */}
+                {/* 4 boxes on the left, big details */}
+            </div>
+            <div>
+                {/* TODO */}
+                {/* Footer */}
+            </div>
+        </div>
+    );
 }
 
-export default Landing
+export default Landing;
