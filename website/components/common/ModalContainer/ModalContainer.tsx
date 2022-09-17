449976776389975bc52f7553a8194aba742328f3
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-09-17 16:47:13 +0530

    Modal Container component

---
+++ website/components/common/ModalContainer/ModalContainer.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./ModalContainer.css";
+
+const ModalContainer = ({
+  isOpen = true,
+  children,
+  className,
+  close,
+  padding,
+  style,
+}: any) => {
+  const modstyle = { padding: padding, ...style };
+  const [onOutside, setOnOutside] = useState(true);
+
+  return (
+    <>
+      {isOpen && (
+        <div
+          className="modalContainer"
+          onMouseDown={() => onOutside && close()}
+        >
+          <div
+            className={`modalBox ${className}`}
+            style={modstyle}
+            onMouseEnter={() => setOnOutside(false)}
+            onMouseLeave={() => setOnOutside(true)}
+          >
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ModalContainer;
